f295645509ca187309314e9f8503d0d1dbe2890c
manishpanda25 <manishpanda259@gmail.com>
2026-02-06 22:14:05 -0500

    add and get all category impliments.

---
+++ src/main/java/com/becoder/entity/BaseModel.java
@@ -0,0 +1,23 @@
+package com.becoder.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseModel {
+	
+	private boolean isActive;
+	private boolean isDeleted;
+	private Integer createdBy;
+	private Date createdOn;
+	private Integer updatedBy;
+	private Date updatedOn;
+	
+	
+
+}
