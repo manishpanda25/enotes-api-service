00d9ec9dc7592b6aeb15364e27565a8fbf8c3c71
manishpanda25 <manishpanda259@gmail.com>
2026-02-08 18:15:58 -0500

    exception handling success..

--- src/main/java/com/becoder/service/CategoryService.java
+++ src/main/java/com/becoder/service/CategoryService.java
@@ -12,7 +12,7 @@ public interface CategoryService {
 	public boolean saveCategory(CategoryDto categoryDto);
 	public List<CategoryDto> getAllCategory();
 	public List<CategoryResponse> getActiveCategory();
-	public CategoryDto getCategoryById(Integer id);
+	public CategoryDto getCategoryById(Integer id) throws Exception;
 	public Boolean deleteCategory(Integer id);
 	
 
